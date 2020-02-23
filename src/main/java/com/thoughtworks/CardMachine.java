b867edd625e69c4588d69b5c9fa33f637996c084
meishaaaaa <meishaaaaa@163.com>
2020-02-23 21:05:09 +0800

    change2

--- src/main/java/com/thoughtworks/CardMachine.java
+++ src/main/java/com/thoughtworks/CardMachine.java
@@ -1,50 +1,50 @@
 package com.thoughtworks;
 
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.Random;
 
 public class CardMachine {
-    private String name;
+    private ArrayList<Card> cardSet = null;
 
     public CardMachine() {
     }
 
-    public CardMachine(String name) {
-        this.name = name;
+    public void setCardSet() {
+        cardSet = new ArrayList<>();
+        String[] color = {"黑桃", "红桃", "方块", "梅花" };
+        String[] num = {"A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K" };
+        for (String string : color) {
+            for (String numb : num) {
+                Card card = new Card(string, numb);
+                cardSet.add(card);
+            }
+        }
+        this.cardSet = cardSet;
     }
 
-    public String getName() {
-        return name;
+    public ArrayList<Card> getCardSet() {
+        return cardSet;
     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
+    public String drawCard(int intNum) {
+        CardMachine cardMachine = new CardMachine();
+        cardMachine.setCardSet();
 
-    public void drawCard(String[] name, int intNum) {
-        intNum -= 1;
+        String sum = null;
         if (intNum > 52 | intNum <= 0) {
-            System.out.println("输入值大于牌组总数，请输入小于等于52的正整数");
+            sum = "输入值大于牌组总数，请输入小于等于52的正整数";
         } else {
-            int[] drawNum = randomArray(intNum, 52);
-            System.out.println(Arrays.toString(draw(name, drawNum)));
+            ArrayList<Card> selectedCard = new ArrayList<>();
+            for (int i = 0; i < intNum; i++) {
+                Random r = new Random();
+                int drawNum = r.nextInt(cardMachine.getCardSet().size());
+                selectedCard.add(cardMachine.getCardSet().get(drawNum));
+                cardMachine.getCardSet().remove(drawNum);
+            }
 
+            sum = selectedCard.toString();
         }
-    }
-
-    public String[] draw(String[] name, int[] num) {
-        String[] draw = new String[num.length];
-        for (int i = 0; i < num.length; i++) {
-            draw[i] = name[num[i]];
-        }
-        return draw;
-    }
-
-    public static int[] randomArray(int len, int max) {
-        int[] arr = new int[len];
-        for (int i = 0; i < len; i++) {
-            arr[i] = (int) (Math.random() * max);
-        }
-        return arr;
+        return sum;
     }
 }
 
